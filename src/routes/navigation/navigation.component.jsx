444ea83a2bf6d47ea96c455bf99b18ca318911b7
jyotirmoy1997 <subhodas45036@gmail.com>
2023-08-17 17:50:25 +0530

    Fixed Responsiveness in Navbar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,9 +4,18 @@ import "./navigation.styles.css"
 import logo from "../../assets/logo.png"
 import { Link } from "react-router-dom"
 import { useNavigate } from "react-router"
+import { useRef } from 'react';
+import { GrClose } from 'react-icons/gr';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navigation = () => {
     const navigate = useNavigate()
+    const navRef = useRef()
+    const showNavBar = () => {
+        // console.log("1")
+        navRef.current.classList.toggle("responsive-nav")
+    }
+
     return(
         <Fragment>
             <div className="nav-wrapper">
@@ -14,14 +23,22 @@ const Navigation = () => {
                     {/* Add Signature Here */}
                     <img src={logo} alt="" height={100} width={164} />
                 </Link>
-                <div className="nav-links">
-                    <Link to="/">Home</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/certifications">Certifications</Link>
+                <div  ref={navRef} className="nav-links-container">
+                    <div className="nav-links">
+                        <Link to="/" onClick={showNavBar}>Home</Link>
+                        <Link to="/projects" onClick={showNavBar} >Projects</Link>
+                        <Link to="/certifications" onClick={showNavBar} >Certifications</Link>
+                    </div>
+                    <div>
+                        <button id="hire-me-btn" onClick={() => navigate("/contact")}>Hire Me</button>
+                    </div>
+                    <button className="nav-btn nav-close-btn" onClick={showNavBar} >
+                        <GrClose/>
+                    </button>
                 </div>
-                <div>
-                    <button id="hire-me-btn" onClick={() => navigate("/contact")}>Hire Me</button>
-                </div>
+                <button className="nav-btn" onClick={showNavBar}>
+                    <GiHamburgerMenu/>
+                </button>
             </div>
             <Outlet/>
         </Fragment>
